d69e4b4901a305f91f6f56620b1780b415485890
Dyno Hongjun Fu <dyno.fu@gmail.com>
2014-01-20 00:30:22 -0800

    simplified version LMK

--- livermore.py
+++ livermore.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
+import traceback
 import sys
+import math
 import logging
 from os.path import exists
 from datetime import date
@@ -40,19 +42,79 @@ class ATRCalculator(object):
 
         return self.atr
 
+# cannot be redefined to other value
+BAND_UPWARD        = 6
+BAND_NATURAL_RALLY = 5
+BAND_SECOND_RALLY  = 4
+BAND_SECOND_REACT  = 3
+BAND_NATURAL_REACT = 2
+BAND_DOWNWARD      = 1
+
+TREND_UPWARD    = 1
+TREND_DOWNWARD  = 2
 class LivermoreMarketKey(object):
     def __init__(self):
-        pass
+        # support and resistance line
+        self.upward_resistance = None
+        self.rally_resistance = None
+        self.react_support = None
+        self.downward_support = None
+
+        self.trend = None
 
     def __call__(self, tick):
-        pass
-        return band
+        current_price = tick["Close"]
+
+        try:
+            if self.trend is None:
+                self.trend = TREND_UPWARD
+                self.upward_resistance = tick["High"]
+                #self.downward_support = tick["Low"]
+                self.band_width = tick["ATR"]
+                #in case we donnot have High-Low data
+                if self.band_width < 0.001:
+                    self.trend = None
+                    return BAND_NATURAL_RALLY
+
+            if self.trend == TREND_UPWARD:
+                level = 6 - int(math.ceil((self.upward_resistance - current_price) / (self.band_width / 6)))
+            if self.trend == TREND_DOWNWARD:
+                level = int(math.ceil((current_price - self.downward_support) / (self.band_width / 6)))
+
+            if level >= BAND_UPWARD:
+                level = BAND_UPWARD
+                #if self.upward_resistance < current_price:
+                #    self.upward_resistance = current_price
+                self.upward_resistance = current_price
+                self.trend = TREND_UPWARD
+
+            if level <= BAND_DOWNWARD:
+                level = BAND_DOWNWARD
+                #if (self.downward_support is None) or (self.downward_support > current_price):
+                #    self.downward_support = current_price
+                self.downward_support = current_price
+                self.trend = TREND_DOWNWARD
+
+            self.level = level
+            self.band_width = tick["ATR"]
+
+            #print "%s \n %s" % (tick, repr(self.__dict__))
+            return self.level
+
+        except Exception, e:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            print exc_type, exc_value
+            #traceback.print_tb(exc_traceback, limit=10, file=sys.stdout)
+            print repr(traceback.extract_tb(exc_traceback))
+
+        print "not here! stk=%s tick=%s" % (repr(self.__dict__), repr(tick))
+        sys.exit(0)
 
 class Stock(object):
     def __init__(self, name):
         self.name = name
 
-    def retrieve_history(self, use_cache=True, start="4/1/2010"):
+    def retrieve_history(self, use_cache=True, start="12/1/2013", end=date.today()):
         store_name = "{}.hd5".format(self.name)
 
         if use_cache and exists(store_name):
@@ -60,7 +122,7 @@ class Stock(object):
             self.history = self.store.get("history")
             return
 
-        self.history = DataReader(self.name, "yahoo", start, date.today())
+        self.history = DataReader(self.name, "yahoo", start, end)
         self.store = HDFStore(store_name)
         self.store.put("history", self.history)
         self.store.flush()
@@ -70,18 +132,18 @@ class Stock(object):
         self.history["ATR"] = self.history.apply(calculator, axis=1)
 
     def process_livermore_market_key(self):
-        pass
+        lmk = LivermoreMarketKey()
+        self.history["level"] = self.history.apply(lmk, axis=1)
 
     def plot_livermore_trend(self):
         # http://matplotlib.org/examples/pylab_examples/color_by_yvalue.html
-        return
 
         close = self.history["Close"]
         atr = self.history["ATR"]
-        tLow = self.history["tLow"]
-        up_trend = ma.masked_where(close > (tLow + 4.0 / 6 * atr), close)
-        dn_trend = ma.masked_where(close < (tLow + 2.0 / 6 * atr), close)
-        between =  ma.masked_where((close >= (tLow + 2.0 / 6 * atr)) & (close <= (tLow + 4.0 / 6 * atr)), close)
+        level = self.history["level"]
+        up_trend = ma.masked_where(level < BAND_UPWARD, close)
+        dn_trend = ma.masked_where(level > BAND_DOWNWARD, close)
+        between =  ma.masked_where((level ==  BAND_DOWNWARD) | (level == BAND_UPWARD), close)
 
         mondays = WeekdayLocator(MONDAY)
         months  = MonthLocator(range(1,13), bymonthday=1, interval=1) # every month
@@ -92,14 +154,18 @@ class Stock(object):
         ax.xaxis.set_minor_locator(mondays)
         ax.grid(True)
 
-        plt.plot(self.history.index, up_trend, "r", self.history.index, dn_trend, "g", self.history.index, between, "b")
+        plt.plot(self.history.index, up_trend, "green", self.history.index, dn_trend, "red", self.history.index, between, "blue")
         plt.show()
 
 def main():
-    #stk = Stock("000001.SS")
-    #stk = Stock("000826.SZ")
-    stk = Stock("QQQ")
-    stk.retrieve_history(start="4/1/2010")
+    stk = Stock("000001.SS")
+    #stk.retrieve_history(use_cache=False, start="1/1/2007", end="1/1/2011")
+    stk = Stock("000826.SZ")
+    stk = Stock("300027.SZ")
+    #stk = Stock("QQQ")
+    #stk = Stock("VMW")
+    #stk = Stock("GOOG")
+    stk.retrieve_history(use_cache=False, start="4/19/2009")
     #stk.histkory = stk.histkory.loc["5/30/2008":]
     stk.process_atr()
     stk.process_livermore_market_key()
