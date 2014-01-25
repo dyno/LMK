10958e505fa6666dca5bc293acdce68d39605753
Dyno Hongjun Fu <dyno.fu@gmail.com>
2014-01-25 10:14:25 -0800

    Rally upward, Reaction downward

--- 1938_1940.py
+++ 1938_1940.py
@@ -10,13 +10,13 @@ import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator, WeekdayLocator, DateFormatter, MONDAY, FRIDAY
 
 UD = 6
-NN = 5
-SN = 4
-SY = 3
-NY = 2
+NY = 5
+SY = 4
+SN = 3
+NN = 2
 DD = 1
 
-band_dict = {"UD":6, "NN":5, "SN":4, "SY":3, "NY":2, "DD":1}
+band_dict = {"UD":6, "NY":5, "SY":4, "SN":3, "NN":2, "DD":1}
 
 ## ETL ##
 dates = pd.date_range(start='19380101', end='19400217', freq="D")
@@ -24,7 +24,7 @@ df = pd.DataFrame(index=dates, columns=("US_band", "US_price", "US_pivotal",
                                         "BS_band", "BS_price", "BS_pivotal",
                                         "Key_band", "Key_price", "Key_pivotal"))
 
-#1938/01/01   NY 65.75*, UP 57*,    UD 122.75*
+#1938/01/01   NN65.75*, UP 57*,    UD 122.75*
 ptn = re.compile(r"""
 (\d+/\d+/\d+)\s+                       # DATE
 (?:([DNPSUY]{2})\s+([0-9.]+)(\*?))*,   # U.S. STEEL
@@ -55,59 +55,59 @@ df.apply(validate, axis=1)
 
 ## plot ##
 style_dict = {
-            DD  : "rv",
-            NY  : "c>",
-            SY  : "c*",
-            SN  : "m*",
-            NN  : "m<",
-            UD  : "g^",
+            DD : "rv",
+            NN : "m>",
+            SN : "m*",
+            SY : "c*",
+            NY : "c<",
+            UD : "g^",
         }
 
-name = "US"
-name = "Key"
-name = "BS"
-price = df["%s_price" % name]
-band = df["%s_band" % name]
-pivotal = df["%s_pivotal" % name]
+for idx, name in enumerate(["Key", "US", "BS"]):
+    price = df["%s_price" % name]
+    band = df["%s_band" % name]
+    pivotal = df["%s_pivotal" % name]
+    alpha = 1.0 - idx * .3
 
-ax = plt.gca()
-ax.set_xmargin(0.02)
-ax.set_ymargin(0.01)
+    ax = plt.gca()
+    ax.set_xmargin(0.02)
+    ax.set_ymargin(0.01)
 
-days = WeekdayLocator(MONDAY)
-months = MonthLocator(range(1, 13), bymonthday=1, interval=1) # every month
-monthsFmt = DateFormatter("%b")
-dayFmt = DateFormatter("%d")
-ax.xaxis.set_major_locator(months)
-ax.xaxis.set_major_formatter(monthsFmt)
-ax.xaxis.set_minor_locator(days)
-ax.xaxis.set_minor_formatter(dayFmt)
-ax.xaxis.grid(False, which='major')
-ax.xaxis.grid(True, which='minor')
+    days = WeekdayLocator(MONDAY)
+    months = MonthLocator(range(1, 13), bymonthday=1, interval=1) # every month
+    monthsFmt = DateFormatter("%b")
+    dayFmt = DateFormatter("%d")
+    ax.xaxis.set_major_locator(months)
+    ax.xaxis.set_major_formatter(monthsFmt)
+    ax.xaxis.set_minor_locator(days)
+    ax.grid(True)
+    #ax.xaxis.set_minor_formatter(dayFmt)
+    ax.xaxis.grid(False, which='major')
+    ax.xaxis.grid(True, which='minor')
 
-for level in range(DD, UD + 1):
+    for level in range(DD, UD + 1):
+        mask = ma.make_mask(df.index)
+        mask = ma.masked_where(band == level, mask)
+        chosen = ma.masked_where(~mask.mask, price)
+        #if chosen.any():
+        plt.plot(df.index, chosen, style_dict[level], alpha=alpha)
+
+    # upward trend
     mask = ma.make_mask(df.index)
-    mask = ma.masked_where(band == level, mask)
+    mask = ma.masked_where((band == NY) | (band == SY) | (band == UD), mask)
     chosen = ma.masked_where(~mask.mask, price)
-    #if chosen.any():
-    plt.plot(df.index, chosen, style_dict[level])
+    plt.plot(df.index, chosen, "g-", alpha=alpha)
 
-# upward trend
-mask = ma.make_mask(df.index)
-mask = ma.masked_where((band == NY) | (band == SY) | (band == UD), mask)
-chosen = ma.masked_where(~mask.mask, price)
-plt.plot(df.index, chosen, "g-")
+    # downward trend
+    mask = ma.make_mask(df.index)
+    mask = ma.masked_where((band == NN) | (band == SN) | (band == DD), mask)
+    chosen = ma.masked_where(~mask.mask, price)
+    plt.plot(df.index, chosen, "r-", alpha=alpha)
 
-# downward trend
-mask = ma.make_mask(df.index)
-mask = ma.masked_where((band == NN) | (band == SN) | (band == DD), mask)
-chosen = ma.masked_where(~mask.mask, price)
-plt.plot(df.index, chosen, "r-")
-
-# pivotal
-mask = ma.make_mask(df.index)
-mask = ma.masked_where(pivotal == True, mask)
-chosen = ma.masked_where(~mask.mask, price)
-plt.plot(df.index, chosen, "bo", alpha=.3)
+    # pivotal
+    mask = ma.make_mask(df.index)
+    mask = ma.masked_where(pivotal == True, mask)
+    chosen = ma.masked_where(~mask.mask, price)
+    plt.plot(df.index, chosen, "bo", alpha=.3 * alpha)
 
 plt.show()
