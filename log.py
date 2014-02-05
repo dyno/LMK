e318873f2c7d7ba18328d2c9f937cc4954a61d48
Dyno Hongjun Fu <dyno.fu@gmail.com>
2014-02-04 22:11:06 -0800

    fix the ipython logging problem

--- log.py
+++ log.py
@@ -3,9 +3,10 @@ import logging
 
 logger = None
 
-def init():
-    logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+def init(loglevel=logging.DEBUG):
+    logging.basicConfig(level=loglevel, format="%(message)s")
     stdoutStreamHandler = logging.StreamHandler(stream=sys.stdout)
 
     global logger
     logger = logging.getLogger()
+    logger.setLevel(loglevel)
