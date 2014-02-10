7484a846687a2f1365ed8a67efe060837eeb83d3
Dyno Hongjun Fu <dyno.fu@gmail.com>
2014-02-10 10:37:51 -0800

    add run_ipynb.sh

---
+++ run_ipynb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ipython notebook --pprint --matplotlib=inline
+
+exit 0
+##
+
+[hfu@hfu-mbp:livermore]$ pip freeze
+Cython==0.20
+Jinja2==2.7.2
+MarkupSafe==0.18
+PyRSS2Gen==1.0.0
+Twisted==12.2.0
+altgraph==0.10.1
+backports.ssl-match-hostname==3.4.0.2
+bdist-mpkg==0.4.4
+bonjour-py==0.3
+git-remote-helpers==0.1.0
+ipython==1.1.0		#<---
+macholib==1.5
+matplotlib==1.1.1	#<---
+modulegraph==0.10.1
+numexpr==2.3
+numpy==1.8.0		#<---
+pandas==0.13.0		#<---
+py2app==0.7.1
+pyOpenSSL==0.13
+python-dateutil==2.2
+pytz==2013.9
+pyzmq==14.0.1
+scipy==0.11.0
+six==1.5.2
+tables==3.1.0
+tornado==3.2
+vincent==0.4.2
+virtualenv==1.11.2
+wsgiref==0.1.2
+xattr==0.6.4
+zope.interface==3.8.0
+
