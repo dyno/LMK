75587ae40cc911e54e28adc3bedff3758570a28b
Dyno Hongjun Fu <dyno.fu@gmail.com>
2014-07-14 00:01:10 -0700

    parse hint to use different datasource

--- test.py
+++ test.py
@@ -1,35 +1,77 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 
-from one import Environment, Stock, PivotCalculator
+import logging
+from datetime import timedelta
+from one import Environment, Stock
 
 _env = Environment()
+_env.init_log(loglevel=logging.INFO)
+_env.init_log(loglevel=logging.DEBUG)
 print "Are we using proxy? %s" % ("Yes" if _env.probe_proxy() else "No", )
 
+check_list = True
 for symbol in (
-               #"^IXIC",
-               #"000001.SS",
-               #"TSLA",
-               "ANET",
                "---",
+               "399102.SZ",
                "300011.SZ", #鼎汉技术
-              ):
-    if symbol.startswith("---"): break
-    stk = Stock(symbol)
-    print "%s => %s" % (symbol, stk.name)
-    stk.retrieve_history("2014-01-01", _env._today)
-    #stk.retrieve_history("2014-01-01", "2014-05-17")
+               "300150.SZ", #世纪瑞尔
+               "-x-",
+               "000001.SS",
+               "---",
+               "600547.SS", #山东黄金
+               "300049.SZ", #福瑞医疗
+               "000788.SZ", #北大医疗
+               "---",
+               "^IXIC", "TSLA",
+               "GGAL", "SSRI",
+               "SINA", "YOKU",
+               "AMBA", "HIMX",
+               "---"
+               "ANET", #"XNET",
+               "-x-",
+               "WETF#ds=google",
+               ):
+    if symbol.startswith("--"):
+        check_list = False; continue
+    if symbol.startswith("-x-"):
+        check_list = True; continue
+    if not check_list: continue
+
+    hint = {}
+    if "#" in symbol:
+        symbol, cmt = symbol.split("#")
+        for e in cmt.split(","):
+            try:
+                k,v = e.split("=")
+                hint[k] = v
+            except:
+                pass
+
+    ds = hint.get("ds")
+    if ds:
+        stk = Stock(symbol, ds=ds)
+    else:
+        stk = Stock(symbol)
+
+    #print "%s => %s" % (symbol, stk.name)
+    end = _env._today
+    #end = (_env.today - timedelta(1)).strftime("%Y-%m-%d")
+    stk.retrieve_history("2014-01-01", end)
     #stk.process_history(freq="W-MON")
     stk.process_history(freq="D")
     h = stk.history
-    n = 2
-    print "head(n)..."
-    print h.head(n)
-    print "tail(n) ..."
-    print h.tail(n)
+    print h.tail(2)
 
-    #stk.visualize("HLC,BAND,PV,PVL")
-    #stk.visualize("PV,PVL", fluct_factor=.15)
-    #stk.visualize("HLC,PV,PVL", fluct_factor=.15)
-    #stk.visualize("BAND,PV,PVL", fluct_factor=0.2)
-    stk.visualize("HLC,PV,PVL,EE,ODR", fluct_factor=0.15)
+    def index(symbol):
+        if symbol.startswith("^"): return True
+        if symbol.endswith("SS") and symbol.startswith("00"): return True
+        if symbol.endswith("SZ") and symbol.startswith("399"): return True
+        return False
+
+    fluct_factor = .1 if index(stk.symbol) else .6
+    #stk.visualize("HLC,PV,PVL,EE,ODR", fluct_factor=fluct_factor)
+    stk.visualize("HLC,PV,PVL,EE,ODR", fluct_factor=fluct_factor)
+    #stk.visualize("BAND,BANDL,PV,EE,ODR", fluct_factor=fluct_factor)
+    #stk.visualize()
+
