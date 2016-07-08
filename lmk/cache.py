17e73b2ec6abec668c33fafc7a5e4ca82c684a42
Dyno Fu <dyno.fu@gmail.com>
2016-07-07 21:37:57 -0700

    fix cache logic error

--- lmk/cache.py
+++ lmk/cache.py
@@ -17,9 +17,10 @@ class Cache:
         self.fn = join(cache_dir, "lmk.hd5")
         with HDFStore(self.fn) as cache:
             if TABLE_RANGE in cache:
-                self.range = cache.get(TABLE_RANGE).astype(date)
+                self.range = cache.get(TABLE_RANGE)
             else:
-                self.range = DataFrame(columns=["start", "end"])
+                self.range = DataFrame({"start": Series([], dtype="datetime64[ns]"),
+                                        "end": Series([], dtype="datetime64[ns]")})
 
             if TABLE_NAME in cache:
                 self.name = cache.get(TABLE_NAME)
@@ -36,7 +37,8 @@ class Cache:
         table = self._table_name(symbol)
 
         if symbol in self.range.index:
-            _start, _end = self.range.loc[symbol]
+            # Timestamp -> date
+            _start, _end = [ts.date() for ts in self.range.loc[symbol]]
             # cache hit - part of existing data.
             if _start <= start <= end <= _end:
                 with HDFStore(self.fn) as cache:
@@ -49,25 +51,26 @@ class Cache:
         table = self._table_name(symbol)
 
         if symbol not in self.range.index:
-            self.range.loc[symbol] = start, end
+            self.range.loc[symbol] = Series([start, end]).astype(datetime64)
 
             with HDFStore(self.fn) as cache:
                 cache.put(table, history)
-                cache.put(TABLE_RANGE, self.range.astype(datetime64))
+                cache.put(TABLE_RANGE, self.range)
 
         else:
-            _start, _end = self.range.loc[symbol]
+            _start, _end = [ts.date() for ts in self.range.loc[symbol]]
 
             # 1. part of existing cache, nothing need to be done.
             if _start <= start <= end <= _end:
                 return
 
             # 2. there is overlap
-            elif _start > start and _end > end:
+            elif (_start > start and _end > end) or (_start < start and _end < end):
                 with HDFStore(self.fn) as cache:
                     h = cache.get(table)
                     h.loc[start:end] = history
-                    self.range.loc[symbol] = start, _end
+                    self.range.loc[symbol] = (start if _start > start else _start,
+                                              end if _end < end else _end)
 
                     cache.put(table, h)
                     cache.put(TABLE_RANGE, self.range.astype(datetime64))
@@ -75,11 +78,11 @@ class Cache:
             # 3. no overlap, save the recent data.
             else:
                 if _end < start: # new data is more recent.
-                    self.range.loc[symbol] = start, end
+                    self.range.loc[symbol] = Series([start, end]).astype(datetime64)
 
                     with HDFStore(self.fn) as cache:
                         cache.put(table, history)
-                        cache.put(TABLE_RANGE, self.range.astype(datetime64))
+                        cache.put(TABLE_RANGE, self.range)
 
     def flush_name(self):
         with HDFStore(self.fn) as cache:
